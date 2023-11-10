c883fd4642be82da5a3e63f67e91bbe98e46130d
Adriano Ramos <onairda.somar@gmail.com>
2023-11-10 08:47:41 -0500

    Initial Commit

---
+++ Fetch Take Home/Fetch_Take_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Take_HomeApp.swift
+//  Fetch Take Home
+//
+//  Created by Adriano Ramos on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Take_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
