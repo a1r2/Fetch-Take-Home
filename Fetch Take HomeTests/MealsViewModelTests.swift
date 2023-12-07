e5a09f87b2e2806c55b76f1863a0f9a454b4b399
Adriano Ramos <onairda.somar@gmail.com>
2023-12-06 21:37:59 -0500

    add test to state
    MealsViewModelTestes
    
    Fix
    
    Fix

--- Fetch Take HomeTests/MealsViewModelTests.swift
+++ Fetch Take HomeTests/MealsViewModelTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import Combine
 @testable import Fetch_Take_Home
 
 final class MealsViewModelTests: XCTestCase {
@@ -87,4 +88,40 @@ final class MealsViewModelTests: XCTestCase {
         XCTAssertEqual(expectedOrder, viewModel.meals)
     }
 
+    func test_State_Error_WhenFetching() async {
+        // Arrange
+        let mock = MockMealServiceProtocol(outputError: NSError(domain: "domain", code: 1, userInfo: [NSLocalizedDescriptionKey : "some error"]))
+        let viewModel = MealsViewModel(service: mock)
+        
+        var cancellables = Set<AnyCancellable>()
+        var changedToError = false
+        var changedErrorMessage = false
+        
+        viewModel.$state
+            .dropFirst()
+            .sink { state in
+                dump(state)
+                if state == .error {
+                    changedToError = true
+                }
+            }
+            .store(in: &cancellables)
+        
+        viewModel.$errorMessage
+            .dropFirst()
+            .sink { errorMessage in
+                dump(errorMessage)
+                if errorMessage != nil {
+                    changedErrorMessage = true
+                }
+            }
+            .store(in: &cancellables)
+        
+        // Act
+        await viewModel.fetch()
+        
+        // Assert
+        XCTAssertTrue(changedToError)
+        XCTAssertTrue(changedErrorMessage)
+    }
 }
